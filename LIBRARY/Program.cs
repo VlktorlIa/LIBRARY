2c523e18aaf157ebe8724f40177addac8d302029
VlktorlIa <v.malykh2006@gmail.com>
2024-04-24 13:51:10 +0200

    Add project files.

---
+++ LIBRARY/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LIBRARY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! Welcome to library app.");
+            Console.WriteLine("Please select action:");
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Get list of books");
+            Console.WriteLine("3. Search books");
+
+            string action = Console.ReadLine();
+
+            Console.WriteLine(action);
+        }
+    }
+}
